2daadd019245866b4bf80bd51f1981528bcbe8f2
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-09 17:54:09 +0100

    Update: Refactor CORS Middleware Configuration
    - Removed redundant `app.use(cors())` call.
    - Refactored CORS configuration to specify `origin: true` and `credentials: true` for better control over cross-origin requests.

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ import adminRoutes from "./routes/adminRoutes.js";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
