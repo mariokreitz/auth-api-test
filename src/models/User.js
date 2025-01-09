825b1e04e1b859ac548aec6c8c3eb82c8193c4e9
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-09 10:13:58 +0100

    Delete: Removed password hashing middleware from userSchema
    The password hashing logic in `userSchema.pre("save")` has been removed.

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -14,11 +13,5 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next;
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
 
 export default mongoose.model("User", userSchema);
