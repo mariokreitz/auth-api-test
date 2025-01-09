f5d8b0a380b1065c576c6b13a86d57c328a4312e
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-09 06:32:47 +0100

    Add: Register authentication routes in app.js
    Added `app.use("/auth", authRoutes)` to include authentication routes for user registration and login.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -8,6 +9,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Auth API is up and running!");
