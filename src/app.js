633ae53b2bdcff7809d2b0bae89cae45035068cc
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-13 13:48:30 +0100

    feat: Integrate session routes in app.js
    - Imported `sessionRoutes` from `./routes/sessionRoutes.js`.
    - Added middleware to mount `sessionRoutes` at `/session`.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use("/session", sessionRoutes);
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/admin", adminRoutes);
